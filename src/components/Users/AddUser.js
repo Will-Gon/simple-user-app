dc8385a7e0744b17fae3e8c4a6917b183b53d9d6
Will-Gon <will.colocho@gmail.com>
2021-07-12 23:02:32 -0700

    add form

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,5 +1,19 @@
 import React from 'react';
 
-const AddUser = () => {};
+const AddUser = (props) => {
+    const addUserHandler = (e) => {
+        e.preventDefault();
+    };
+
+    return (
+        <form onSubmit={addUserHandler}>
+            <label htmlFor="username">Username</label>
+            <input id="username" type="text" />
+            <label htmlFor="Age">Age (Years)</label>
+            <input id="Age" type="number" />
+            <button type="submit">Add User</button>
+        </form>
+    );
+};
 
 export default AddUser;
