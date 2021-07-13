8bc52e7bc00783fdfc374f769cc3d0e00e87817b
Will-Gon <will.colocho@gmail.com>
2021-07-13 14:05:16 -0700

    add button component/styling

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,18 +1,24 @@
 import React from 'react';
 
+import Card from '../UI/Card';
+import classes from './AddUser.module.css';
+import Button from '../UI/Button';
+
 const AddUser = (props) => {
     const addUserHandler = (e) => {
         e.preventDefault();
     };
 
     return (
-        <form onSubmit={addUserHandler}>
-            <label htmlFor="username">Username</label>
-            <input id="username" type="text" />
-            <label htmlFor="Age">Age (Years)</label>
-            <input id="Age" type="number" />
-            <button type="submit">Add User</button>
-        </form>
+        <Card className={classes.input}>
+            <form onSubmit={addUserHandler}>
+                <label htmlFor="username">Username</label>
+                <input id="username" type="text" />
+                <label htmlFor="Age">Age (Years)</label>
+                <input id="Age" type="number" />
+                <Button type="submit">Add User</Button>
+            </form>
+        </Card>
     );
 };
 
